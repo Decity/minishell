2d4cb4a64c1850ed3d6206cff6bba47b8c8e8b3b
Racer911-1 <douwe.bakker.03@gmail.com>
2025-10-05 14:29:59 +0200

    Added a function to find the environmental variable name Added a function (incomplete) to split the PATH value.

--- src/execution.c
+++ src/execution.c
@@ -15,6 +15,55 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * @brief Find @p `name` in the environment @p `envp`.
+ *
+ * The function will only check each line of @p `envp` equal to the length
+ * of @p `name`. So truncating the NAME may return unexpected results.
+ *
+ * @param[in]	name	The NAME to find.
+ * @param[in]	envp	The environment.
+ *
+ * @return Pointer to the first occurrence of NAME found in @p `envp`,
+ * @return or NULL on failure.
+ */
+char	*get_env_name(const char *name, const char **envp)
+{
+	size_t	i;
+	size_t	name_len;
+
+	i = 0;
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+	name_len = ft_strlen(name);
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], name, name_len))
+		{
+			return (envp[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+
+char	*get_path(char *path)
+{
+	char	**paths;
+
+
+	paths = ft_split(path, ':');
+	// How do we error handle if NULL? Should we exit()?
+	for (size_t i = 0; paths[i]; i++)
+	{
+		printf("%s\n", paths[i]);
+	}
+	return (NULL);
+}
+
 void	execute(t_data *data)
 {
 	pid_t	pid;
@@ -23,6 +72,12 @@ void	execute(t_data *data)
 	if (DEBUG)
 		printf("=== execute_command() ===\n\n");
 
+	char *test = get_env_name("PATH", data->envp);
+	if (test == NULL)
+	{
+		return ;
+	}
+	get_path(test);
 	pid = fork();
 	if (pid == 0)
 	{
