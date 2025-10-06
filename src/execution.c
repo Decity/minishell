9945559e495a3e3584cd1eec264b8402ce04f187
Racer911dash1 <douwe.bakker.03@gmail.com>
2025-10-06 15:29:58 +0200

    Added get_executable_path that finds the binary from the user input

--- src/execution.c
+++ src/execution.c
@@ -6,7 +6,7 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 12:02:01 by ebelle            #+#    #+#             */
-/*   Updated: 2025/10/03 13:39:49 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/10/06 15:29:42 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,13 @@ char	*get_executable_path(const char *exec)
 	size_t	i;
 
 	paths = ft_split(getenv("PATH"), ':');
+	// TODO: Handle malloc fail.
 	i = 0;
-	// TODO: handle malloc fails. Do we return NULL? Do we do exit()?
 	while (paths[i])
 	{
 		slashed_path = ft_strjoin(paths[i], "/");
 		abs_path = ft_strjoin(slashed_path, exec);
-		// TODO: handle malloc fails. Also maybe a better method to add slashes at the end?
+		// TODO: handle malloc fail.
 		free(slashed_path);
 		if (access(abs_path, X_OK) == 0)
 		{
@@ -53,12 +53,13 @@ char	*get_executable_path(const char *exec)
 void	execute(t_data *data)
 {
 	pid_t	pid;
+	char	*path;
 	int		fd;
 
 	if (DEBUG)
 		printf("=== execute_command() ===\n\n");
 
-	get_executable_path("blap");
+	path = get_executable_path(data->command->arguments[0]);
 	pid = fork();
 	if (pid == 0)
 	{
@@ -67,7 +68,7 @@ void	execute(t_data *data)
 		dup2(fd, STDOUT_FILENO);
 		close(fd);
 
-		execve("/usr/bin/ls", &data->command->arguments[0], data->command->arguments);
+		execve(path, &data->command->arguments[0], data->command->arguments);
 		// execvp(data->command->arguments[0], data->command->arguments);
 	}
 	else
