861ca2583f770064d213301eebe8a6b43aa6cb8b
Racer911dash1 <douwe.bakker.03@gmail.com>
2025-10-17 18:09:21 +0200

    Checkpoint commit cd built-in should update the information for env and the struct

--- src/built-ins/cd.c
+++ src/built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:30:29 by dbakker           #+#    #+#             */
-/*   Updated: 2025/10/17 15:27:10 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/10/17 17:55:43 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,25 @@
 /**
  * @brief Change directory and update the member variables of @p `directory`.
  *
+ * @param[out]	list		Update `VALUE` of `PWD` and `OLDPWD`.
  * @param[out]	directory	Member variables to update.
  * @param[in]	path		Path to go to.
+ *
+ * @return `0` on success, or `-1` on failure.
  */
-int	ed_change_directory(t_pwd *directory, const char *path)
+int	ed_change_directory(t_list *list, t_pwd *directory, const char *path)
 {
+	char	*pwd;
+	char	*old_pwd;
+
 	if (chdir(path) == 0)
 	{
+		// Disregard the lack of NULL checks
 		ed_update_pwd(directory);
+		pwd = ft_strjoin("PWD=", directory->pwd);
+		old_pwd = ft_strjoin("OLDPWD=", directory->old_pwd);
+		export_env(list, pwd);
+		export_env(list, old_pwd);
 		return (0);
 	}
 	return (-1);
