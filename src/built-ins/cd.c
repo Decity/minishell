b6484e07c3ab0df774212777620baaa4e4d631e5
Racer911-1 <douwe.bakker.03@gmail.com>
2025-10-19 13:07:52 +0200

    Checkpoint Commit Added some NULL checks

--- src/built-ins/cd.c
+++ src/built-ins/cd.c
@@ -6,20 +6,23 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:30:29 by dbakker           #+#    #+#             */
-/*   Updated: 2025/10/17 17:55:43 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/10/18 22:50:14 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @brief Change directory and update the member variables of @p `directory`.
+ * @brief Change directory and update the environmental variables
+ * @brief `PWD` and `OLDPWD`.
  *
  * @param[out]	list		Update `VALUE` of `PWD` and `OLDPWD`.
  * @param[out]	directory	Member variables to update.
  * @param[in]	path		Path to go to.
  *
- * @return `0` on success, or `-1` on failure.
+ * @retval`-1` on failure.
+ * @retval `0` on success.
+ * @retval `1` on malloc failure.
  */
 int	ed_change_directory(t_list *list, t_pwd *directory, const char *path)
 {
@@ -28,12 +31,16 @@ int	ed_change_directory(t_list *list, t_pwd *directory, const char *path)
 
 	if (chdir(path) == 0)
 	{
-		// Disregard the lack of NULL checks
-		ed_update_pwd(directory);
+		if (ed_update_pwd(directory) == NULL)
+			return (1);
 		pwd = ft_strjoin("PWD=", directory->pwd);
 		old_pwd = ft_strjoin("OLDPWD=", directory->old_pwd);
-		export_env(list, pwd);
-		export_env(list, old_pwd);
+		if (pwd == NULL || old_pwd == NULL)
+			return (free(pwd), free(old_pwd), 1);
+		if (export_env(list, pwd) == NULL)
+			return (1);
+		if (export_env(list, old_pwd) == NULL)
+			return (1);
 		return (0);
 	}
 	return (-1);
--- src/built-ins/echo.c
+++ src/built-ins/echo.c
@@ -6,8 +6,26 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:27:44 by dbakker           #+#    #+#             */
-/*   Updated: 2025/10/17 14:38:19 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/10/18 22:49:52 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief Print @p `message` to `stdout`.
+ *
+ * @param[in] message	Message to print
+ * @param[in] option	Extra behaviour how @p `message` should be printed.
+ */
+void	ed_echo(const char *message, const char *option)
+{
+	if (option == NULL)
+	{
+		printf("%s\n", message);
+	}
+	if (ft_memcmp(option, "-n", ft_strlen(option)) == 0)
+	{
+		printf("%s", message);
+	}
+}
