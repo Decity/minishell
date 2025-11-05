6a42eefa9e478c3b95e2c0f331b2acf632cb5fee
Elie Belle <elie.strangelove@gmail.com>
2025-11-05 15:46:23 +0100

    implemented signals into execution, split execution up, added logic for running single builtin

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 12:02:01 by ebelle            #+#    #+#             */
-/*   Updated: 2025/10/29 12:14:38 by elie             ###   ########.fr       */
+/*   Updated: 2025/11/05 15:29:04 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,68 +17,36 @@
 
 void	execution(t_data *data)
 {
-	if (!data->command->next)
-		execute_single_cmd(data->command, llist_to_array(data->envp));
+	if (!data->command->next && is_builtin(data->command->args[0]))
+		execute_single_builtin(data->command, data);
+	else if (!data->command->next)
+		execute_single_cmd(data->command, data);
 	else
 		execute_cmds(data);
 }
 
-void	execute_single_cmd(t_cmd *cmd, char **envp)
+void	execute_single_cmd(t_cmd *cmd, t_data *data)
 {
-	// if (is_built_in())
-	// 	execute_built_in();
-	// else
-		apply_redirections(cmd);
-		execute_binary(cmd, envp);
-}
-
-void	execute_cmds(t_data *data)
-{
-	t_cmd	*current;
-	pid_t	*pids;
-	int		*pipefd;
-	int		prev_pipefd;
 	char	**envp;
-	size_t	i;
+	pid_t	pid;
+	int		status;
 
 	envp = llist_to_array(data->envp);
-	pids = malloc(get_cmds_count(data->command) * sizeof(pid_t));
-	pipefd = malloc (2 * sizeof(int));
-	current = data->command;
-	i = 0;
-	while (current)
+	pid = fork();
+	// handle error
+	if (pid == 0)
 	{
-		if (current->next != NULL)
-			pipe(pipefd);
-
-		pids[i] = fork();
-		if (pids[i] == 0)
-		{
-			setup_child_redirections(pipefd, prev_pipefd, i == 0, current->next == NULL);
-			close_pipes(pipefd, prev_pipefd, i == 0, current->next == NULL);
-			execute_single_cmd(current, envp);
-			exit(1);
-		}
-
-		if (i > 0)
-			close(prev_pipefd);
-		if (current->next != NULL)
-		{
-			prev_pipefd = pipefd[0];
-			close(pipefd[1]);
-		}
-
-		current = current->next;
-		i++;
+		restore_signals_default();
+		apply_redirections(cmd);
+		if (is_builtin(cmd->args[0]))
+			execute_builtin(cmd, data);
+		else
+			execute_binary(cmd, envp);
+		exit(0);
 	}
-	
-	// wait for pids
-	while (i)
-	{
-		wait(NULL);
-		i--;
-	}
-	
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		data->exit_status = WEXITSTATUS(status);
 }
 
 void	execute_binary(t_cmd *cmd, char **envp)
@@ -86,7 +54,7 @@ void	execute_binary(t_cmd *cmd, char **envp)
 	char	*path;
 
 	path = get_executable_path(cmd->args[0]);
+	// handle error
 	execve(path, cmd->args, envp);
 	exit(1);
 }
-
---
+++ src/execution/execution_builtins.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution_builtins.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 15:22:00 by elie              #+#    #+#             */
+/*   Updated: 2025/11/05 15:22:00 by elie             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+#include <unistd.h>
+
+bool	is_builtin(const char *cmd)
+{
+	if (!cmd)
+		return (false);
+	if (ft_strcmp(cmd, "echo") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "cd") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "pwd") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "export") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "unset") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "env") == 0)
+		return (true);
+	if (ft_strcmp(cmd, "exit") == 0)
+		return (true);
+	return (false);
+}
+
+void	execute_builtin(t_cmd *cmd, t_data *data)
+{
+	if (ft_strcmp(cmd->args[0], "echo") == 0)
+		ed_echo((const char **)cmd->args + 1);
+	if (ft_strcmp(cmd->args[0], "cd") == 0)
+		ed_change_directory(data->envp, &data->directory, cmd->args[1]);
+	if (ft_strcmp(cmd->args[0], "pwd") == 0)
+		pwd_print(data->directory);
+	if (ft_strcmp(cmd->args[0], "export") == 0)
+	{
+		if (cmd->args[1])
+			export_env(data->envp, cmd->args[1]);
+		else
+			export_print(data->envp);
+	}
+	if (ft_strcmp(cmd->args[0], "unset") == 0)
+		unset_env(&data->envp, cmd->args[1]);
+	if (ft_strcmp(cmd->args[0], "env") == 0)
+		env_print(data->envp);
+	if (ft_strcmp(cmd->args[0], "exit") == 0)
+		minishell_exit();
+}
+
+void	execute_single_builtin(t_cmd *cmd, t_data *data)
+{
+	int		saved_stdin;
+	int		saved_stdout;
+
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	apply_redirections(cmd);
+	execute_builtin(cmd, data);
+	dup2(saved_stdin, STDIN_FILENO);
+	dup2(saved_stdout, STDOUT_FILENO);
+	close(saved_stdin);
+	close(saved_stdout);
+}
