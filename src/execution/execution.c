68df3fdaf977f5dd9f88c9ad18ab9758a35d12f7
Elie Belle <elie.strangelove@gmail.com>
2025-11-10 15:42:21 +0100

    now checks for full path

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 12:02:01 by ebelle            #+#    #+#             */
-/*   Updated: 2025/11/10 14:37:54 by elie             ###   ########.fr       */
+/*   Updated: 2025/11/10 15:41:04 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,10 @@ void	execute_binary(t_cmd *cmd, t_list *envp)
 {
 	char	*path;
 
-	path = get_executable_path(cmd->args[0], envp);
+	if (cmd->args[0][0] == '/' || ft_strncmp(cmd->args[0], "./", 2) == 0 || ft_strncmp(cmd->args[0], "../", 3) == 0)
+		path = cmd->args[0];
+	else
+		path = get_executable_path(cmd->args[0], envp);
 	// handle error
 	execve(path, cmd->args, llist_to_array(envp));
 	exit(1);
