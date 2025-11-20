8ac3fcd926b69832435edf337636990933def302
Racer911dash1 <douwe.bakker.03@gmail.com>
2025-11-20 16:01:59 +0100

    Breaking up big function into smaller functions

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 10:53:02 by dbakker           #+#    #+#             */
-/*   Updated: 2025/11/20 13:13:01 by elie             ###   ########.fr       */
+/*   Updated: 2025/11/20 15:56:05 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ void	*init_heredoc(t_cmd *cmd)
 	}
 	return (cmd);
 }
+
 /**
  * The warning it prints out if you CTRL + D in the heredoc.
  */
@@ -84,49 +85,72 @@ void	heredoc_print_warning(size_t line_count, const char *delimiter)
 	ft_putstr_fd("')\n", STDERR_FILENO);
 }
 
-t_data	*heredoc(t_data *data)
+bool	heredoc_check(t_data *data)
 {
-	static size_t	line_count = 0;
-	size_t			i;
-	char			*line;
-
-	if (DEBUG)
-		printf("=== Heredoc begin ===\n");
 	if (count_redir_heredoc((const char **)data->tokens) > HEREDOC_LIMIT)
 	{
-		return (NULL);
+		return (false);
 	}
 	if (init_heredoc(data->command) == NULL)
 	{
-		return (NULL);
+		return (false);
 	}
-	i = 0;
-	while (data->command->redirect.infile[i].file)
+	return (true);
+}
+
+void	heredoc_readline(t_cmd *cmd, size_t i)
+{
+	static size_t	line_count = 0;
+	char			*line;
+
+	while (true != false)
 	{
-		if (data->command->redirect.infile[i].redir_type == TYPE_REDIRECTION_HEREDOC)
+		line = readline(NULL);
+		line_count++;
+		if (line == NULL)
 		{
-			data->command->redirect.infile[i].fd = open(data->command->redirect.infile[i].file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			while (true)
-			{
-				line = readline("  ");
-				line_count++;
-				if (line == NULL)
-				{
-					heredoc_print_warning(line_count, data->command->redirect.infile[i].delimiter);
-					break ;
-				}
-				if (ft_strcmp(line, data->command->redirect.infile[i].delimiter) == 0)
-				{
-					free(line);
-					break ;
-				}
-				ft_putendl_fd(line, data->command->redirect.infile[i].fd);
-				free(line);
-			}
-			close(data->command->redirect.infile[i].fd);
+			heredoc_print_warning(line_count, cmd->redirect.infile[i].delimiter);
+			break ;
+		}
+		if (strcmp(line, cmd->redirect.infile[i].delimiter) == 0)
+		{
+			free(line);
+			break ;
+		}
+		ft_putendl_fd(line, cmd->redirect.infile[i].fd);
+		free(line);
+	}
+}
+
+void	*heredoc_file_insertion(t_cmd *cmd)
+{
+	size_t			i;
+
+	i = 0;
+	while (cmd->redirect.infile[i].file)
+	{
+		if (cmd->redirect.infile[i].redir_type == TYPE_REDIRECTION_HEREDOC)
+		{
+			cmd->redirect.infile[i].fd = open(cmd->redirect.infile[i].file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (cmd->redirect.infile[i].fd)
+				return (NULL);
+			heredoc_readline(cmd, i);
+			close(cmd->redirect.infile[i].fd);
 		}
 		i++;
 	}
+	return (cmd);
+}
+
+t_data	*heredoc(t_data *data)
+{
+	if (DEBUG)
+		printf("=== Heredoc begin ===\n");
+
+	if (heredoc_check(data) == false)
+		return (NULL);
+
+	heredoc_file_insertion(data->command);
 	if (DEBUG)
 		printf("=== Heredoc end ===\n");
 	return (data);
