979678308f81c055b6b6292dfa689462fcc4d8b8
Racer911dash1 <douwe.bakker.03@gmail.com>
2025-11-20 10:00:25 +0100

    Remove out-of-scope handling of curly braces

--- src/expansion/get_param.c
+++ src/expansion/get_param.c
@@ -6,7 +6,7 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 18:05:06 by elie              #+#    #+#             */
-/*   Updated: 2025/11/19 15:33:46 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/11/20 09:24:01 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,12 @@ char	*get_parameter_var(const char *str)
 	if (!str || str[0] != '$')
 		return (NULL);
 
-	// Handle ${VAR} format
-	if (str[1] == '{')
-	{
-		i = 2;
-		while (str[i] && str[i] != '}')
-			i++;
-		if (str[i] != '}')
-			return (NULL);
-		end = i + 1;
-	}
-	// Handle $VAR format (alphanumeric + underscore)
-	else
-	{
-		i = 1;
-		if (!ft_isalpha(str[i]) && str[i] != '_')
-			return (NULL);
-		while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
-			i++;
-		end = i;
-	}
+	i = 1;
+	if (!ft_isalpha(str[i]) && str[i] != '_')
+		return (NULL);
+	while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
+		i++;
+	end = i;
 
 	result = ft_calloc(end + 1, sizeof(char));
 	if (!result)
@@ -74,28 +60,13 @@ char	*get_parameter_name(const char *str)
 	if (!str || str[0] != '$')
 		return (NULL);
 
-	// Handle ${VAR} format
-	if (str[1] == '{')
-	{
-		i = 2;
-		start = i;
-		while (str[i] && str[i] != '}')
-			i++;
-		if (str[i] != '}')
-			return (NULL);
-		end = i;
-	}
-	// Handle $VAR format
-	else
-	{
-		start = 1;
-		i = start;
-		if (!ft_isalpha(str[i]) && str[i] != '_')
-			return (NULL);
-		while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
-			i++;
-		end = i;
-	}
+	start = 1;
+	i = start;
+	if (!ft_isalpha(str[i]) && str[i] != '_')
+		return (NULL);
+	while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
+		i++;
+	end = i;
 
 	parameter_name = ft_calloc(end - start + 1, sizeof(char));
 	if (!parameter_name)
