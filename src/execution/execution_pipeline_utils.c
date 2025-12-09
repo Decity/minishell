f4128f330d78f15c78a60370426ccea1e542f0bd
Elie Belle <elie.strangelove@gmail.com>
2025-12-09 13:38:40 +0100

    refactored

--- src/execution/execution_pipeline_utils.c
+++ src/execution/execution_pipeline_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_pipeline_utils.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
+/*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 10:30:00 by elie              #+#    #+#             */
-/*   Updated: 2025/12/08 14:42:36 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/12/09 13:18:33 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,25 @@ void	close_parent_pipes(size_t i, int prev_pipefd, int *pipefd,
 		close(prev_pipefd);
 	if (current->next != NULL)
 		close(pipefd[1]);
+}
+
+/**
+ * @brief Close pipe file descriptors
+ *
+ * Closes previous pipe read end and current pipe both ends if applicable.
+ *
+ * @param[in] pipefd Current pipe file descriptors
+ * @param[in] prev_pipefd Previous pipe read end
+ * @param[in] is_first True if first command in pipeline
+ * @param[in] is_last True if last command in pipeline
+ */
+void	close_pipes(int *pipefd, int prev_pipefd, bool is_first, bool is_last)
+{
+	if (!is_first)
+		close(prev_pipefd);
+	if (!is_last)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
 }
