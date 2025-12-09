0ca15cb9011d0885464cf94b331e31aac6f7c04c
Racer911dash1 <douwe.bakker.03@gmail.com>
2025-12-09 16:52:21 +0100

    Added newline at exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: dbakker <dbakker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:29:42 by dbakker           #+#    #+#             */
-/*   Updated: 2025/12/01 09:52:42 by dbakker          ###   ########.fr       */
+/*   Updated: 2025/12/09 16:51:18 by dbakker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	minishell_exit(t_data *data)
 {
-	ft_putstr_fd("exit", STDERR_FILENO);
+	ft_putendl_fd("exit", STDERR_FILENO);
 	exit_cleanup(data, data->exit_status);
 }
